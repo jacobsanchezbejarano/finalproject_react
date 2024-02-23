aa692297ffaf2f56dbbe59e96b51b72312229c49
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-02-23 11:40:50 -0400

    udpate navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,23 @@
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
 export default function Nav(props) {
+    const { pathname } = useLocation();
+    const [currentPath, setCurrentPath] = useState(pathname);
+
+    useEffect(() => {
+        setCurrentPath(pathname);
+    }, [pathname]);
 
     return (
         <nav>
             <ul id="primaryNav" className={props.activo ? 'open' : ''}>
-                <li><Link to="/" activeClassName="selected" className="nav-item">Home</Link></li>
-                <li><Link to="/about" activeClassName="selected" className="nav-item">About</Link></li>
-                <li><Link to="/menu" activeClassName="selected" className="nav-item">Menu</Link></li>
-                <li><Link to="/reservations" activeClassName="selected" className="nav-item">Reservations</Link></li>
-                <li><Link to="/order" activeClassName="selected" className="nav-item">Order Online</Link></li>
-                <li><Link to="/login" activeClassName="selected" className="nav-item">Login</Link></li>
+                <li><Link to="/" className={`nav-item ${currentPath === '/' ? 'selected' : ''}`}>Home</Link></li>
+                <li><Link to="/about" className={`nav-item ${currentPath === '/about' ? 'selected' : ''}`}>About</Link></li>
+                <li><Link to="/menu" className={`nav-item ${currentPath === '/menu' ? 'selected' : ''}`}>Menu</Link></li>
+                <li><Link to="/reservations" className={`nav-item ${currentPath === '/reservations' ? 'selected' : ''}`}>Reservations</Link></li>
+                <li><Link to="/order" className={`nav-item ${currentPath === '/order' ? 'selected' : ''}`}>Order Online</Link></li>
+                <li><Link to="/login" className={`nav-item ${currentPath === '/login' ? 'selected' : ''}`}>Login</Link></li>
             </ul>
         </nav>
     )
