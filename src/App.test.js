373d442aafd75886226395594569f6110d646d69
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-02-22 23:06:12 -0400

    unit testing updated: form validation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import Home from './components/Home';
 import Card from "./components/Card";
 import { reducer, reserve } from './utils';
-
+import BookingForm from './components/BookingForm';
+import { AlertProvider } from "./context/alertContext";
 
 test('Renders the home screen', () => {
     render(<Home />);
@@ -70,49 +71,95 @@ test("Returns an empty array for an unknown date", () => {
   // Assert
   expect(newState).toEqual([]);
 });
-describe('Tests for local storage', () => {
-  beforeEach(() => {
-    localStorage.clear();
+
+
+test('should save a reservation to localStorage', () => {
+  localStorage.clear();
+  const reservationData = {
+    date: '2024-02-22',
+    time: '18:00',
+    guests: '5',
+    occasion: 'Birthday',
+    comment: 'Special instructions',
+  };
+
+  reserve(reservationData);
+
+  expect(localStorage.getItem('reservations')).not.toBeNull();
+
+  const storedReservations = JSON.parse(localStorage.getItem('reservations'));
+  expect(storedReservations).toEqual([reservationData]);
+});
+
+test('should add multiple reservations to localStorage', () => {
+  localStorage.clear();
+  const reservationData1 = {
+    date: '2024-02-22',
+    time: '18:00',
+    guests: '5',
+    occasion: 'Birthday',
+    comment: 'Special instructions',
+  };
+
+  const reservationData2 = {
+    date: '2024-02-23',
+    time: '19:00',
+    guests: '2',
+    occasion: 'Anniversary',
+    comment: 'No special instructions',
+  };
+
+  reserve(reservationData1);
+  reserve(reservationData2);
+
+  const storedReservations = JSON.parse(localStorage.getItem('reservations'));
+  expect(storedReservations).toEqual([reservationData1, reservationData2]);
+});
+
+
+test('displays error messages when form fields are empty', async () => {
+  const availableTimes = [
+    { id: 1, time: "17:00" },
+    { id: 2, time: "18:00" },
+    { id: 3, time: "19:00" },
+    { id: 4, time: "20:00" },
+    { id: 5, time: "21:00" },
+    { id: 6, time: "22:00" }];
+  render(<AlertProvider><BookingForm availableTimes={availableTimes}/></AlertProvider>);
+
+  fireEvent.submit(screen.getByText('Make Your reservation'));
+
+  await waitFor(() => {
+    expect(screen.getByText('Date is required')).toBeInTheDocument();
+    expect(screen.getByText('Time is required')).toBeInTheDocument();
   });
+});
 
-  test('should save a reservation to localStorage', () => {
-    const reservationData = {
-      date: '2024-02-22',
-      time: '18:00',
-      guests: '5',
-      occasion: 'Birthday',
-      comment: 'Special instructions',
-    };
+test('does not display error messages when form fields are filled correctly', async () => {
+  const availableTimes = [
+    { id: 1, time: "17:00" },
+    { id: 2, time: "18:00" },
+    { id: 3, time: "19:00" },
+    { id: 4, time: "20:00" },
+    { id: 5, time: "21:00" },
+    { id: 6, time: "22:00" }
+  ];
 
-    reserve(reservationData);
+  render(
+    <AlertProvider>
+      <BookingForm availableTimes={availableTimes}/>
+    </AlertProvider>
+  );
 
-    expect(localStorage.getItem('reservations')).not.toBeNull();
+  fireEvent.change(screen.getByLabelText('Choose date *'), { target: { value: '2024-02-22' } });
+  fireEvent.change(screen.getByLabelText('Choose time *'), { target: { value: '18:00' } });
+  fireEvent.change(screen.getByLabelText('Number of guests'), { target: { value: '5' } });
+  fireEvent.change(screen.getByLabelText('Occasion'), { target: { value: 'Birthday' } });
 
-    const storedReservations = JSON.parse(localStorage.getItem('reservations'));
-    expect(storedReservations).toEqual([reservationData]);
-  });
+  fireEvent.submit(screen.getByText('Make Your reservation'));
 
-  test('should add multiple reservations to localStorage', () => {
-    const reservationData1 = {
-      date: '2024-02-22',
-      time: '18:00',
-      guests: '5',
-      occasion: 'Birthday',
-      comment: 'Special instructions',
-    };
-
-    const reservationData2 = {
-      date: '2024-02-23',
-      time: '19:00',
-      guests: '2',
-      occasion: 'Anniversary',
-      comment: 'No special instructions',
-    };
-
-    reserve(reservationData1);
-    reserve(reservationData2);
-
-    const storedReservations = JSON.parse(localStorage.getItem('reservations'));
-    expect([storedReservations]).toEqual([reservationData1, reservationData2]);
+  await waitFor(() => {
+    expect(screen.queryByText('Date is required')).toBeNull();
+    expect(screen.queryByText('Time is required')).toBeNull();
   });
 });
