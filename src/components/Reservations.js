9e8f943c46b7dfd32279bdf7bec8429195d1e294
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-02-22 22:13:05 -0400

    updated layout reservations

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,14 +1,17 @@
 import BookingForm from "./BookingForm";
 import ReservationTable from './ReservationTable';
 import FullScreenSection from './FullScreenSection';
+import { Flex } from "@chakra-ui/react";
 
 export default function Reservations(props) {
     return (
         <FullScreenSection
             justifyContent="center"
             alignItems="center">
-                <BookingForm availableTimes={props.availableTimes} updateTimes={props.updateTimes}/>
-                <ReservationTable/>
+                <Flex>
+                    <BookingForm availableTimes={props.availableTimes} updateTimes={props.updateTimes}/>
+                    <ReservationTable/>
+                </Flex>
         </FullScreenSection>
     )
 }
